f443c3a03cb0b3e8de38c61da5788a846517f809
potukanumajeevitha7 <potukanumajeevitha7@gmail.com>
2022-02-06 20:40:43 +0530

    DAY 10 Assignments

---
+++ DAY 10 Assignments/day 10 evening example/day 10 evening example/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_10_evening_example
+{
+    abstract class College
+    {
+        public string CollegeName()
+        {
+            return "VIT";
+        }
+        public string CollegeLocation()
+        {
+            return "MADANAPALLI" ;
+        }
+        public abstract int StudentID();
+        public abstract string StudentName();
+    }
+    class EEE : College
+    {
+        public override int StudentID()
+        {
+            return 1;
+        }
+        public override string StudentName()
+        {
+            return "john";
+        }
+    }
+    class ECE : College
+    {
+        public override int StudentID()
+       
+        {
+            return 2;
+        }
+        public override string StudentName()
+        {
+            return "jeevitha";
+        }
+    }
+    class MECH : College
+    {
+        public override int StudentID()
+        {
+            return 3;
+        }
+        public override string StudentName()
+        {
+            return "mouni" ;
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Details Entered Successfully");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DAY 10 Assignments/day 10 evening/day 10 evening/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_10_evening
+{
+    abstract class Salary
+    {
+        public int GetPF(int basic)
+        {
+            return 12 * basic / 100;
+        }
+        public int GetHRA(int basic)
+        {
+            return 40 * basic / 100;
+        }
+        public abstract int GetCA();
+        public abstract int GetSA();
+    }
+    class microsoft : Salary
+    {
+        public override int GetCA()
+        {
+            return 25000;
+        }
+        public override int GetSA()
+        {
+            return 10000;
+        }
+    }
+    class Google : Salary
+    {
+        public override int GetCA()
+        {
+            return 32000;
+        }
+        public override int GetSA()
+        {
+            return 56000;
+        }
+    }
+    class IBM : Salary
+    {
+        public override int GetCA()
+        {
+            return 45000;
+        }
+        public override int GetSA()
+        {
+            return 50000;
+        }
+    }
+    class facebook : Salary
+    {
+        public override int GetCA()
+        {
+            return 1000;
+        }
+        public override int GetSA()
+        {
+            return 3000;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //microsoft
+            //Google
+            //IBM
+            //facebook
+            Console.WriteLine("completed processing");
+            Console.ReadLine();
+        }
+    }
+}
