7248678ead29d336cbbdfaf41f60868bb2797cdc
potukanumajeevitha7 <potukanumajeevitha7@gmail.com>
2022-02-15 18:14:23 +0530

    DAY 17 Assignments

---
+++ DAY 17 Assignments/Project 1/johneyLibrary/MyBaseClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//*******************************************************
+  //  Author: JEEVITHA
+    //Purpose:access the modifiers within assembly
+//**************************************************************
+namespace johneyLibrary
+{
+    public class MyBaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        //********WITHIN CLASS**********
+        public void MyBaseClassMethod()
+        {
+            
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;  
+            e = 5;
+            
+        }
+    }
+    public class MyDerivedClass : MyBaseClass
+    {
+        //************DERIVED CLASS**************
+        public void MyDerivedClassMethod()
+        {
+            
+            a = 1;
+            b = 2;
+            c = 3;
+            d = 4;
+            e = 5;
+            
+        }
+    }
+    public class MyOtherClass
+    {
+        //***********OTHER CLASS**************
+        public void MyOtherClassMethod()
+        {
+            MyBaseClass mb =new MyBaseClass();
+            
+            mb.a = 1;
+            mb.b = 2;
+            mb.c = 3;
+            mb.d = 4;
+            mb.e = 5;
+            
+        }
+    }
+}
---
+++ DAY 17 Assignments/Project 1/publicLibrary/PublicLibraryDerivedClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using johneyLibrary;
+//************************************************************
+  //  Author: JEEVITHA
+    //Purpose: access the modifiers OTHER ASSEMBLY
+//***************************************************************
+namespace publicLibrary
+{
+    public class PublicLibraryDerivedClass : MyBaseClass
+    {
+        //***********DERIVED CLASS************
+        public void PublicLibraryDerivedClassMethod()
+        {
+            a = 1;
+            b=2;
+            c=3;
+            d=4;
+            e=5;
+        }
+    }
+    public class PublicLibraryOtherClass
+    {
+        //*************OTHER CLASS***************
+        public void PublicLibraryOtherClassMethod()
+        {
+            MyBaseClass mb = new MyBaseClass();
+            mb.a = 1;
+            mb.b = 2;
+            mb.c = 3;
+            mb.d = 4;
+            mb.e = 5;
+
+        }
+    }
+}
