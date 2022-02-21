9ac691245d3567a22dc6909822e5d6ebef887b89
potukanumajeevitha7 <potukanumajeevitha7@gmail.com>
2022-02-21 12:37:55 +0530

    Day 21 Assignment

---
+++ DAY 21 Assignment/MYWEBAPPLICATION/MYWEBAPPLICATION/WebService1.asmx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+//***********************************************************
+//Author: JEEVITHA
+//Purpose: Create web service for mathematical operation
+//         Factorial, Add, Mul, Div
+//************************************************************
+
+namespace MYWEBAPPLICATION
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Factorial(int n)
+        {
+            int fact = 1, i;
+            for (i = 1; i <= n; i++)
+            fact = fact * i;
+            return fact;
+        }
+
+        [WebMethod]
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+
+        [WebMethod]
+        public int Div(int a, int b)
+        {
+            return a/b;
+        }
+    }
+}
---
+++ DAY 21 Assignment/MYWEBAPPLICATION/Mathematics/Program.cs
@@ -0,0 +1,30 @@
+﻿using Mathematics.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//***************************************************
+// Author: JEEVITHA
+// Purpose: Console Application and consume Web Application
+//***************************************************
+
+namespace Mathematics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient obj = new WebService1SoapClient();
+            Console.WriteLine("*********** FACTORIAL OF A NUM **********");
+            Console.WriteLine(obj.Factorial(7));
+            Console.WriteLine("********* ADDITION ********");
+            Console.WriteLine(obj.Add(2,4));
+            Console.WriteLine("************ multiplication ***********");
+            Console.WriteLine(obj.Mul(7,8));
+            Console.WriteLine("******** DIVISION ***********");
+            Console.WriteLine(obj.Div(10,2));
+            Console.ReadLine();
+        }
+    }
+}
